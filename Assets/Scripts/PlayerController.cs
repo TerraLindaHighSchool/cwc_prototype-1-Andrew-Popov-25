81a35338ca6cadc737d1b3a7545ff5461b9ee2d0
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2022-03-22 15:41:51 -0700

    Tutorial 1.4 Completed - 03/22/22

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 5f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
