8e2a5bd788044489101b2c43684d43a01b954545
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2022-03-24 15:22:44 -0700

    Final Commit - Challenge 1 (Plane Programming)
    Committed, pushed, and ready to grade.

---
+++ Assets/Scripts/PropellerSpinner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Spins the propeller
+ * 03/24/22
+ */
+
+public class PropellerSpinner : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y - 1, transform.rotation.z) * Time.deltaTime * speed);
+    }
+}
